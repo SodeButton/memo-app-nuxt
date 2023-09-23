ff6520707b7b83d91751edb9aa588060808466e4
Button501 <51501719+SodeButton@users.noreply.github.com>
2023-09-23 14:26:30 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h2>メモアプリ</h2>
       </v-col>
       <v-col cols="6" class="text-right">
-        <v-btn @click="create()" >新しいメモ</v-btn>
+        <v-btn @click="createMemo()" >新しいメモ</v-btn>
       </v-col>
       <v-col cols="12">
         <v-text-field
@@ -18,28 +18,47 @@
           append-icon="mdi-magnify"
         ></v-text-field>
       </v-col>
-      <v-col cols="12" class="text-center" v-if="loading">
+      <v-col cols="12" class="text-center" v-if="isLoading">
         <v-progress-circular
           indeterminate
           size="50"
           width="5"
         ></v-progress-circular>
       </v-col>
-      <v-col cols="12" class="text-center" v-if="memoData.length === 0 && !loading">
+      <v-col cols="12" class="text-center" v-if="memoData.length === 0 && !isLoading">
         <p class="grey--text ma-auto mt-10" style="max-width: 280px">上の新しいメモボタンをクリックしてメモを作成します</p>
       </v-col>
-      <v-col v-for="(item, index) in memoData" :v-key="index" cols="12" v-if="memoData.length !== 0 && !loading">
+      <v-col v-for="(item, index) in memoData" :v-key="index" cols="12" v-if="memoData.length !== 0 && !isLoading">
         <v-card>
           <v-sheet
             max-width="100%"
             height="4px"
             color="yellow darken-1"
           ></v-sheet>
-          <v-card-actions>
-            <v-btn plain class="align-content-end"><v-icon>mdi-dots-horizontal</v-icon></v-btn>
+          <v-card-actions style="width: 100%; position:relative;">
+            <v-menu :offset-y="true" left bottom :close-on-click="true">
+              <template v-slot:activator="{on, attrs}">
+                <v-btn plain class="" :ripple="false" v-bind="attrs" v-on="on" icon absolute top right style="top:0; right:0;">
+                  <v-icon>mdi-dots-horizontal</v-icon>
+                </v-btn>
+              </template>
+              <v-list class="align-center">
+                <v-list-item link :href="'/edit?id='+item.id">
+                  <v-list-item-icon><v-icon>mdi-note-edit-outline</v-icon></v-list-item-icon>
+                  <v-list-item-title>メモを編集</v-list-item-title>
+                </v-list-item>
+                <v-list-item link href="/" @click="deleteMemo(item.id)">
+                  <v-list-item-icon><v-icon>mdi-trash-can-outline</v-icon></v-list-item-icon>
+                  <v-list-item-title>メモの削除</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </v-card-actions>
-          <v-card-text class="white--text " v-html="item.text">
-
+          <v-card-text class="white--text">
+            <div v-for="(text, index) in item.texts" v-if="index <= 10">
+              <span v-if="index < 10">{{text}}<br></span>
+              <span v-if="index === 10">...</span>
+            </div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -53,33 +72,50 @@ export default {
   name: 'IndexPage',
   data() {
     return {
+      textData: [],
       memoData: [],
-      loading: true,
+      isLoading: true,
       search: '',
     }
   },
   methods: {
-    create() {
+    createMemo() {
       this.$router.push('/create');
     },
+    deleteMemo(id) {
+      const db = this.$fire.firestore;
+      const memoCollection = db.collection("memoData");
+      try {
+        memoCollection.doc(id).delete();
+        this.isLoading = true;
+      } catch (e) {
+        console.log(e);
+      }
+    },
   },
-  mounted() {
+  created() {
     const db = this.$fire.firestore;
     const memoCollection = db.collection("memoData");
 
-    memoCollection.orderBy("lastUpdate", "desc").onSnapshot((snapshot) => {
-      const items = [];
+    try {
+      memoCollection.orderBy("lastUpdate", "desc").onSnapshot((snapshot) => {
+        const items = [];
+        snapshot.forEach((doc) => {
+          const texts = doc.data().text.split("\n");
+          console.log(texts);
+          items.push({texts: texts, id: doc.id, lastUpdate: doc.data().lastUpdate});
+        });
 
-      snapshot.forEach((doc) => {
-        items.push({text: doc.data().text.replaceAll('\n','<br>')});
+        this.memoData = items;
+        this.isLoading = false;
       });
-
-      this.memoData = items;
-    });
+    } catch (e) {
+      console.log(e);
+    }
   },
   watch: {
     memoData: function () {
-      this.loading = false;
+      this.isLoading = false;
     }
   }
 }
